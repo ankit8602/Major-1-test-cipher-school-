b088c2406df734d515b3c7e88cbe46e3802193a2
ankit8602 <87423118+ankit8602@users.noreply.github.com>
2022-01-24 10:44:28 +0530

    Add files via upload

---
+++ Coding Question 1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def TwoSum(lst,n,target):
+    for i in range(len(n)):
+        for j in range(i+1, len(n)):
+            if lst[i]+lst[j] == target:
+                return[i,j]
+n = input()
+lst = [int(item) for item in n.split()]
+target = int(input())
+TwoSum(lst,n,target)
+
